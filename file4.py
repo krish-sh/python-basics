35b42ca620acbc9c8912d4e54e168e0d4e419df8
Krish Sharma <krishsharma3355@gmail.com>
2026-06-24 12:47:31 +0530

    Interest and Shopping calculator

---
+++ file4.py
@@ -0,0 +1,28 @@
+#  Interset calculator
+
+# principle = float(input("Enter the principle amount:"))
+# rate = float(input("Enter the Rate of Interest %:"))
+# time = int(input("Enter the time in years:"))
+
+
+# interest = (principle * rate * time) / 100
+
+# print("The interest is:", interest)
+
+# Shopping calculator
+
+
+item1 = input("Entet the item1 name:")
+price1 = float(input("Enter the price1 of the item:"))
+
+item2 = input("Entet the item2 name:")
+price2 = float(input("Enter the price2 of the item:"))
+
+
+
+total =  + price1 + price2  
+
+print("The items you have purchased are:",  item1, item2)
+print("The total price of the items is:", total)
+print("After adding the Gst of 18% the total price is:", total + (total * 18 / 100))
+print("After adding the discount of 10% the total price is:", total - (total * 10 / 100))
