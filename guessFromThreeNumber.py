4b40e06c7248eaf119e3dd386974e948c79b85fc
Krish Sharma <krishsharma3355@gmail.com>
2026-06-25 12:33:23 +0530

    Guess the Number Game

---
+++ guessFromThreeNumber.py
@@ -0,0 +1,16 @@
+num = 7
+Guess = int(input("Guess First number between 1 to 10: "))
+Guess1 = int(input("Guess Second number between 1 to 10: "))
+Guess2 = int(input("Guess Third number between 1 to 10: "))
+
+if num == Guess:
+    print("First number is correct", Guess)
+
+elif num == Guess1:
+        print("Second number is correct", Guess1)
+elif num == Guess2:
+        print("Third number is correct", Guess2)
+else:
+       print("No number is correct")
+
+    
